8663200e86bac38f354bbf2ea9f3f11a3cbe3782
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2019-01-25 12:22:05 +0100

    Configuring the swappable settings in the example project

--- example/example/settings.py
+++ example/example/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FLEXIBLE_PLAN_FEATURE_MODEL = 'flexible_plans.Feature'
+FLEXIBLE_PLAN_PLAN_MODEL = 'flexible_plans.Plan'
