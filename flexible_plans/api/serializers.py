ac02a17e83459c70b8dcce5af5b867cfc8583f64
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2019-05-13 18:37:14 +0200

    Api views

--- flexible_plans/api/serializers.py
+++ flexible_plans/api/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
+from swapper import get_model_name, load_model
 
-from flexible_plans.models import Plan
+Plan = load_model('flexible_plans', 'Plan')
+Subscription = load_model('flexible_plans', 'Subscription')
+Customer = load_model('flexible_plans', 'Customer')
+Feature = load_model('flexible_plans', 'Feature')
+
+
+class FeatureSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Feature
+        fields = '__all__'
 
 
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
+        fields = '__all__'
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
         fields = '__all__'
--- flexible_plans/api/views.py
+++ flexible_plans/api/views.py
@@ -1,10 +1,23 @@
 from rest_framework import viewsets
+from flexible_plans.api.serializers import PlanSerializer, SubscriptionSerializer, CustomerSerializer
+from swapper import load_model
 
-from flexible_plans.api.serializers import PlanSerializer
-from flexible_plans.models import Plan
+
+Plan = load_model("flexible_plans", "Plan")
+Subscription = load_model("flexible_plans", "Subscription")
+Customer = load_model("flexible_plans", "Customer")
 
 
 class PlanViewSet(viewsets.ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
